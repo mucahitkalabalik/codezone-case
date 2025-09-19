baa2494ede4b5b0e3cfefcbe98d676f256d08319
DESKTOP-IHIDJ8G\PC <mucahitkalabalik@outlook.com>
2025-09-19 12:03:34 +0300

    discovery bug fix.

--- src/components/Home/Discovery/Cards.jsx
+++ src/components/Home/Discovery/Cards.jsx
@@ -4,17 +4,17 @@ function Cards() {
   return (
     <div className="mt-10">
       {Data.map((item, index) => (
-        <div key={index} className=" grid grid-cols-12 mb-10">
-          <div className="md:col-span-3 col-span-12 pr-4 mb-10">
-            <img src={item.bg} className="md:h-40 h-30 object-cover" />
-            <div className="md:mt-15  text-[#3B3B3B]">{item.date} </div>
+        <div key={index} className=" grid grid-cols-12 mb-10 md:px-0 px-5 ">
+          <div className="md:col-span-3 col-span-12 pr-4 mb-10 md:block flex flex-col items-center">
+            <img src={item.bg} className="md:h-40 h-50 w-full object-cover" />
+            <div className="md:mt-12  text-[#3B3B3B] text-start w-full">{item.date} </div>
           </div>
           <div className="md:col-span-9 col-span-12">
-            <div className="text-2xl flex items-center space-x-3">
+            <div className="md:text-2xl flex items-center space-x-3 mb-5 md:mb-0">
               <img src={item.img} />
               <div>{item.name}</div>
             </div>
-            <div className="text-[25px] font-bold text-white">
+            <div className="md:text-[25px] font-bold text-white">
               {item.description}
             </div>
             <div className=" border-b border-[#3B3B3B] md:mt-10 mt-5"></div>
