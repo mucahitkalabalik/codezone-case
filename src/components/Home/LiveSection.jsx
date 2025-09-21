7b2c8281dba47152ceb833ed98c740599ffaa682
DESKTOP-IHIDJ8G\PC <mucahitkalabalik@outlook.com>
2025-09-22 02:49:58 +0300

    bug fix

--- src/components/Home/LiveSection.jsx
+++ src/components/Home/LiveSection.jsx
@@ -4,7 +4,7 @@ import LiveSectionBg from "../../../public/images/live-section-bg.png";
 function LiveSection() {
   return (
     <div
-      className="w-full h-screen bg-cover bg-center relative flex flex-col  items-center justify-center md:justify-between md:px-20"
+      className="w-full h-screen bg-cover bg-center relative flex flex-col  items-center justify-center md:justify-between "
       style={{ backgroundImage: `url(${LiveSectionBg.src})` }}
     >
       <img
@@ -12,12 +12,12 @@ function LiveSection() {
         alt="Carousel Detail"
         className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-auto h-45 md:h-80 z-20"
       />
-      <div className="flex flex-col text-center text-white mb-8 md:mb-0 md:absolute md:left-3/12 md:top-1/6 rotate-[-3deg]">
-        <div className="flex mb-10">
+      <div className="flex flex-col text-center text-white mb-8 md:mb-0 md:absolute md:top-1/12   lg:left-4/14   rotate-[-3deg]">
+        <div className="flex mb-10 md:mt-6">
           <img
             src="images/twitch.png"
             alt="Twitch"
-            className="w-32 md:w-128 lg:w-60"
+            className="w-32 md:w-40 lg:w-60"
           />
           <div className=" border-l border-[#3B3B3B] mr-10"></div>
           <div className="text-left">
@@ -35,11 +35,11 @@ function LiveSection() {
 
         <div className="flex items-center justify-center space-x-3 md:space-x-5">
           <span className="flex items-center space-x-2  border border-[#3B3B3B] p-3 rounded-3xl">
-            <button className="bg-[#864CF6] text-white px-6 py-3 rounded-xl flex items-center space-x-2">
+            <button className="bg-[#864CF6] text-white px-3 md:-px-6 py-2 md:py-3 rounded-xl flex items-center space-x-2">
               <img src="/images/like.png" alt="Like" />
               <span>Takip Et</span>
             </button>
-            <button className="bg-[#222123] text-white px-6 py-3 rounded-xl flex items-center space-x-2">
+            <button className="bg-[#222123] text-white px-3 md:-px-6 py-2 md:py-3 rounded-xl flex items-center space-x-2">
               <img src="/images/star.png" alt="Like" className="mr-2" />
               Abone Ol
             </button>
@@ -50,7 +50,7 @@ function LiveSection() {
         <img
           src="/images/man.png"
           alt="Man"
-          className="object-contain w-[200px] md:w-[550px] md:h-auto"
+          className="object-contain w-[200px] md:w-[550px] md:h-auto md:mr-10"
         />
         <img
           src="/images/woman.png"
--- src/components/Home/Trends/TrendCard.jsx
+++ src/components/Home/Trends/TrendCard.jsx
@@ -1,25 +1,35 @@
 "use client";
 import React from "react";
+import Link from "next/link";
 
 function TrendCard({ item }) {
   return (
-    <div className="lg:col-span-4 md:col-span-6 col-span-12  flex flex-col mb-10">
+    <div className="lg:col-span-4 md:col-span-6 col-span-12 flex flex-col mb-10">
       <div className="mr-5 flex">
-        <div className="flex items-start mr-5  text-[50px] text-[#2A2A2A]">{`0${item.id}`}</div>
-        <div className="flex items-start flex-col  mb-5 text-white">
+        <div className="flex items-start mr-5 text-[50px] text-[#2A2A2A]">
+          {`0${item.id}`}
+        </div>
+        <div className="flex items-start flex-col mb-5 text-white">
           <div className="flex items-center justify-start mb-5 mt-5">
-            <img src={item.img} alt="" />
-            <div className="ml-5">{item.name} </div>
+            <img src={item.img} alt={item.name} />
+            <div className="ml-5">{item.name}</div>
           </div>
-          <div className="mb-10 text-[16px] md:text-[24px]  font-bold uppercase md:min-h-[150px]">
+          <div className="mb-10 text-[16px] md:text-[22px] font-bold uppercase md:min-h-[150px]">
             {item.description}
           </div>
           <div className="border border-[#3B3B3B] w-full"></div>
-          <div className="mt-5">Daha Fazla Oku</div>
+          <div className="mt-5">
+            <Link
+              href={`/blog/${item.slug}`}
+          
+            >
+              Daha Fazla Oku
+            </Link>
+          </div>
         </div>
       </div>
     </div>
   );
 }
-// #2A2A2A
+
 export default TrendCard;
